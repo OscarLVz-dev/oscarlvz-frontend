f0d05a0f907cc2acd3ce49770f89698677d73902
OscarLVz <olechugav1311@gmail.com>
2023-05-11 23:24:58 -0600

    Loader adde

---
+++ src/assets/scripts.js
@@ -0,0 +1,9 @@
+(function($){
+	"use strict";
+
+	$(window).on( 'load', function(){
+		/*------------------- PRELOADER ------------------*/
+		$('body').addClass('page-loaded');
+	});
+	
+}(jQuery));
