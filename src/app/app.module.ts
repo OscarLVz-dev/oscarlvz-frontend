6ac3b5b8bd997354ff0614c2d82508ef08777f7f
OscarLVz <olechugav1311@gmail.com>
2023-05-11 18:11:13 -0600

    Clabe copy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClipboardModule } from '@angular/cdk/clipboard'
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ClipboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
