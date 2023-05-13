e54d854c94669f68ecaafebe821483200f9d1cc7
OscarLVz <olechugav1311@gmail.com>
2023-05-13 11:35:33 -0600

    css updated

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
+  selector: 'main-component',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
