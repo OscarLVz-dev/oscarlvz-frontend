6a1e9b20cceb49431f5731694ee0287de0e6a55e
OscarLVz <olechugav1311@gmail.com>
2023-05-13 11:16:34 -0600

    Routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ClipboardModule } from '@angular/cdk/clipboard'
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './components/main/main.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
     ClipboardModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
