6dd78c17608b8f216943e19cf75107f03d93d832
OscarLVz <olechugav1311@gmail.com>
2023-05-15 10:05:58 -0600

    Component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './components/main/main.component';
-import { FooterComponent } from './components/commons/footer/footer.component'
+import { FooterComponent } from './components/commons/footer/footer.component';
+import { WidgetsListComponent } from './components/pages/widgets-list/widgets-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     FooterComponent,
+    WidgetsListComponent,
   ],
   imports: [
     BrowserModule,
