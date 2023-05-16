c336168a32ada24f633a81cf2e6182041d1727f5
OscarLVz <olechugav1311@gmail.com>
2023-05-16 13:12:26 -0600

    Routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { WidgetsListComponent } from './components/pages/widgets-list/widgets-list.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'widgets', component: WidgetsListComponent }
+  { path: 'widgets', component: WidgetsListComponent },
+  { path: 'proyectos', component: ProjectsComponent },
 ];
 
 @NgModule({
